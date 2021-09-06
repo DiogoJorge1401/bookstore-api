6e20a4172cc19e6e544708baa976d5a2ede7d409
Suegoid <diogojorge1401@gmail.com>
2021-09-06 13:14:22 -0300

    refactor

--- src/main/java/com/diogo/Services/DbServicesTest.java
+++ src/main/java/com/diogo/Services/DbServicesTest.java
@@ -18,14 +18,14 @@ public class DbServicesTest {
 	private BookRepository bookRepository;
 
   public void instanceDatabase(){
-    Categoria cat1 = new Categoria(1l, "c1", "d1");
-		Categoria cat2 = new Categoria(2l, "c2", "d2");
-		categoryRepository.saveAll(List.of(cat1,cat2));
-		Livro liv1 = new Livro(1l, "t1", "a1", "t1", cat1);
-		Livro liv2 = new Livro(2l, "t2", "a2", "t2", cat2);
-		Livro liv3 = new Livro(3l, "t3", "a3", "t3", cat1);
-		Livro liv4 = new Livro(4l, "t4", "a4", "t4", cat2);
-		bookRepository.saveAll(List.of(liv1,liv2,liv3,liv4));
+    // Categoria cat1 = new Categoria(1l, "c1", "d1");
+		// Categoria cat2 = new Categoria(2l, "c2", "d2");
+		// categoryRepository.saveAll(List.of(cat1,cat2));
+		// Livro liv1 = new Livro(1l, "t1", "a1", "t1", cat1);
+		// Livro liv2 = new Livro(2l, "t2", "a2", "t2", cat2);
+		// Livro liv3 = new Livro(3l, "t3", "a3", "t3", cat1);
+		// Livro liv4 = new Livro(4l, "t4", "a4", "t4", cat2);
+		// bookRepository.saveAll(List.of(liv1,liv2,liv3,liv4));
 
   }
 }
