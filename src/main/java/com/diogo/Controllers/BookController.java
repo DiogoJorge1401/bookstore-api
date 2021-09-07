72c2a40df643ba271d6b1b39c5de0e709c89b1f9
Suegoid <diogojorge1401@gmail.com>
2021-09-07 10:46:03 -0300

    refactor CrossOrigin

--- src/main/java/com/diogo/Controllers/BookController.java
+++ src/main/java/com/diogo/Controllers/BookController.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin("*","http://localhost:4200")
+@CrossOrigin(origins = {"Access-Control-Allow-Origin", "*","Access-Control-Allow-Methods"})
 @RequestMapping("books")
 public class BookController {
   @Autowired
